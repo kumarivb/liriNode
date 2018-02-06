5055753b125f06fe3425909e4bdf20da601e7c01
kvb <kumarivb@gmail.com>
2018-02-06 12:54:28 -0600

    added spotify, twitter, and request to liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 // add code to read and set any environment variables with the dotenv package
 require("dotenv").config();
+//  API library for the Spotify REST API
+var Spotify = require('node-spotify-api');
+// asynchronous client library for the Twitter REST and Streaming API's
+var Twitter = require('twitter');
+// make http calls
+var request = require('request');
 
 // add the code required to import the `keys.js` file and store it in a variable
 var keys = require("./keys.js");
@@ -7,4 +13,4 @@ var keys = require("./keys.js");
 // access your keys information
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
-
+ 
