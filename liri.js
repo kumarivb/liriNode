aef9d902d29aa017e2804cf2583d2623dd2b6bc1
kvb <kumarivb@gmail.com>
2018-02-06 12:35:57 -0600

    imported keys.js and added access to key information in liri.js

--- liri.js
+++ liri.js
@@ -1,2 +1,10 @@
-// read and set environment variables
+// add code to read and set any environment variables with the dotenv package
 require("dotenv").config();
+
+// add the code required to import the `keys.js` file and store it in a variable
+var keys = require("./keys.js");
+
+// access your keys information
+var spotify = new Spotify(keys.spotify);
+var twitter = new Twitter(keys.twitter);
+
